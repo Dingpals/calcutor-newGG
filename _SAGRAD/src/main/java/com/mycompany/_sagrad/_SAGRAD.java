0d05cfd7d46599eadce39e3ee0eeff667f3eefdf
Dingpals <150411456+Dingpals@users.noreply.github.com>
2023-11-13 15:10:09 +0800

    Add files via upload

---
+++ _SAGRAD/src/main/java/com/mycompany/_sagrad/_SAGRAD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._sagrad;
+
+/**
+ *
+ * @author WS
+ */
+public class _SAGRAD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
